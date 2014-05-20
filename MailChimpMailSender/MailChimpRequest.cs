9786bc805cb63261ab735a282d1557eac8122f54
milenafarf <m.farfulowska@gmail.com>
2014-05-21 01:01:32 +0200

    * MailChimpRequest.cs: komentarze
    * MailChimpResponse.cs: dodano total, html, text

--- MailChimpMailSender/MailChimpRequest.cs
+++ MailChimpMailSender/MailChimpRequest.cs
@@ -32,7 +32,6 @@ namespace MailChimpMailSender
         /// <value>The options.</value>
         [DataMember(Name = "options", EmitDefaultValue = false)]
         public MailChimpOptions Options { get; set; }
-
     }
 
     /// <summary>
@@ -44,7 +43,7 @@ namespace MailChimpMailSender
         /// <summary>
         /// Pobiera lub ustawia id listy odbiorców kampanii.
         /// </summary>
-        /// <value>The list identifier.</value>
+        /// <value>Identyfikator listy odbiorców kampanii</value>
         [DataMember(Name = "list_id", EmitDefaultValue = false)]
         public string ListId { get; set; }
 
@@ -60,11 +59,13 @@ namespace MailChimpMailSender
         /// <summary>
         /// Pobiera lub ustawia nazwa odbiorcy (nie adres email)
         /// </summary>
-        /// <value>The name of the from.</value>
+        /// <value>Nazwa odbiorcy</value>
         [DataMember(Name = "to_name", EmitDefaultValue = false)]
         public string ToName { get; set; }
     }
 
+    /// <summary>
+    /// Zawartość kampanii
     /// </summary>
     [DataContract]
     public class MailChimpContent
