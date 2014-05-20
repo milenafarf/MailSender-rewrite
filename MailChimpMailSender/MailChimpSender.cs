1777ba99a7e8a1fea70fcb88935de37d485ddcd4
milenafarf <m.farfulowska@gmail.com>
2014-05-21 00:18:06 +0200

    * MailChimpRequest.cs: struktura kampani
    * MailChimpMailSender.csproj: struktura kampanii
    
    * MailChimpSender.cs: 
    
    * MandrillRequest.cs: dopisalam FromEmail i FromName + troche
      komentarz
    
    * MandrillSender.cs: srednik dodany

--- MailChimpMailSender/MailChimpSender.cs
+++ MailChimpMailSender/MailChimpSender.cs
@@ -10,27 +10,44 @@ namespace MailChimpMailSender
     using MailSender;
 
     /// <summary>
-    /// Mail chimp sender.
+    /// Klasa pozwalająca na komunikację z serwisem MailChimp
     /// </summary>
     public class MailChimpSender : ISender
     {
+        private readonly string apiUrl = "https://<dc>.api.mailchimp.com/2.0/";
+
+        /// <summary>
+        /// Typ danych przesyłanych do serwera.
+        /// </summary>
+        private readonly string contentType = "application/json; charset=UTF-8";
+
+        /// <summary>
+        /// Pole przechowujące klucz identyfikujący użytkownika usługi MailChimp.
+        /// </summary>
+        private readonly string apiKey;
+
+        /// <summary>
+        /// Pole przechowujące adres nadawcy.
+        /// </summary>
+        private readonly string fromMail;
+
         public MailChimpSender(string apikey)
         {
+            this.apiKey = apikey;
         }
 
         #region ISender implementation
 
-        public Response TestSender ()
+        public Response TestSender()
         {
-            throw new NotImplementedException ();
+            throw new NotImplementedException();
         }
 
-        public Response SendMail (Mail mail)
+        public Response SendMail(Mail mail)
         {
-            throw new NotImplementedException ();
+            throw new NotImplementedException();
         }
 
         #endregion
     }
 }
-
