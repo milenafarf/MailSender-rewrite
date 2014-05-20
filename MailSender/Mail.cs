e2855117877bd3c9f99ef1afae4c6b2c96f65b41
milenafarf <m.farfulowska@gmail.com>
2014-05-20 19:55:34 +0200

    dodano komentarze

--- MailSender/Mail.cs
+++ MailSender/Mail.cs
@@ -14,15 +14,20 @@ namespace MailSender
     /// </summary>
     public class Mail
     {
+		/// <summary>
+		/// Initializes a new instance of the <see cref="MailSender.Mail"/> class.
+		/// </summary>
+		/// <param name="content">Message content</param>
+		/// <param name="subject">Subject.</param>
+		/// <param name="html">If set to <c>true</c> html than message contains html content.</param>
 		public Mail(string content, string subject, bool html)
-		{
+		{			
 			Content = content;
 			Subject = subject;
 			Html = html;
 		}
-
-		public string Content{ get; set; }
-		public string Subject{ get; set; }
+		public string Content { get; set; }
+		public string Subject { get; set; }
 		public bool Html { get; set; }
 	}
 }
--- MandrillMailSender/MandrillResponse.cs
+++ MandrillMailSender/MandrillResponse.cs
@@ -6,28 +6,53 @@
 
 namespace MandrillMailSender
 {
-    using System.Runtime.Serialization;
-    using System.Collections.Generic;
+	using System.Collections.Generic;
+    using System.Runtime.Serialization;    
 
     [DataContract]
     public class MandrillResponse
     {
+
         [DataMember(Name = "status")]
-        public string Status { get; set; }
+		/// <summary>
+		/// Gets or sets the status.
+		/// </summary>
+		/// <value>The status.</value>
+		public string Status { get; set; }
 
         [DataMember(Name = "PING")]
-        public string Ping { get; set; }
+		/// <summary>
+		/// Gets or sets the ping.
+		/// </summary>
+		/// <value>The ping.</value>
+		public string Ping { get; set; }
 
         [DataMember(Name = "message")]
-        public string Messege { get; set; }
+		/// <summary>
+		/// Gets or sets the messege.
+		/// </summary>
+		/// <value>The messege.</value>
+		public string Messege { get; set; }
 
         [DataMember(Name = "name")]
-        public string Name { get; set; }
+		/// <summary>
+		/// Gets or sets the name.
+		/// </summary>
+		/// <value>The name.</value>
+		public string Name { get; set; }
 
         [DataMember(Name = "code")]
-        public int Code { get; set; }
-
+		/// <summary>
+		/// Gets or sets the code.
+		/// </summary>
+		/// <value>The code.</value>
+		public int Code { get; set; }
+	
         [DataMember(Name = "", EmitDefaultValue = false)]
-        public List<MandrillResponse> Responses { get; set; }
+		/// <summary>
+		/// Gets or sets the responses.
+		/// </summary>
+		/// <value>The responses.</value>
+		public List<MandrillResponse> Responses { get; set; }
     }
 }
