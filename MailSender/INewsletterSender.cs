9b49b0fea76582415e5d421a78bf52027dff9610
konto <m.dobrzynski@outlook.com>
2014-05-21 13:22:14 +0200

    Create INewsletterSender.cs

---
+++ MailSender/INewsletterSender.cs
@@ -0,0 +1,12 @@
+// -----------------------------------------------------------------------
+//  <copyright file="ISender.cs" company="m (m.dobrzynski@outlook.com)">
+//      Author: m (m.dobrzynski@outlook.com).
+//  </copyright>
+// -----------------------------------------------------------------------
+
+namespace MailSender
+{
+    interface INewsletterSender
+    {
+    }
+}
