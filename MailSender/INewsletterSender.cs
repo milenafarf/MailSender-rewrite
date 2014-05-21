f93305d1ea218b06b02068e75589c81713f5af7b
konto <m.dobrzynski@outlook.com>
2014-05-21 13:37:01 +0200

    Update INewsletterSender.cs

--- MailSender/INewsletterSender.cs
+++ MailSender/INewsletterSender.cs
@@ -1,12 +1,28 @@
 // -----------------------------------------------------------------------
-//  <copyright file="INewsletterSender.cs" company="m (m.dobrzynski@outlook.com)">
+//  <copyright file="ISender.cs" company="m (m.dobrzynski@outlook.com)">
 //      Author: m (m.dobrzynski@outlook.com).
 //  </copyright>
 // -----------------------------------------------------------------------
 
 namespace MailSender
 {
+    using System.Collections.Generic;
+    
     interface INewsletterSender
     {
+        string CreateReceiversList(List<Receiver> receivers);
+
+        List<Receiver> GetReceiversLists();
+
+        string CreateCampaign(string listid, Mail mail);
+
+        string StartCampaign(string campaignid);
+
+        string PauseCampaign(string campaignid);
+
+        string ResumeCampaign(string campaignid);
+
+        string SearchCampaign(string query);
+
     }
 }
