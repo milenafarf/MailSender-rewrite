45fa0a8d8d70d258e5a74247efcf0d67b3f23a54
konto <m.dobrzynski@outlook.com>
2014-05-23 00:33:09 +0200

    Poczatek przenoszenia serializerow do odzielnych klas

---
+++ MailSenderHelpers/JsonDeserializer.cs
@@ -0,0 +1,15 @@
+// -----------------------------------------------------------------------
+//  <copyright file="JsonDeserializer.cs" company="m (m.dobrzynski@outlook.com)">
+//      Author: m (m.dobrzynski@outlook.com).
+//  </copyright>
+// -----------------------------------------------------------------------
+
+namespace MailSenderHelpers
+{
+    public class JsonDeserializer<T>
+    {
+        public T Deserialize(string json)
+        {
+        }
+    }
+}
---
+++ MailSenderHelpers/JsonSerializer.cs
@@ -0,0 +1,18 @@
+// -----------------------------------------------------------------------
+//  <copyright file="JsonSerializer.cs" company="m (m.dobrzynski@outlook.com)">
+//      Author: m (m.dobrzynski@outlook.com).
+//  </copyright>
+// -----------------------------------------------------------------------
+
+namespace MailSenderHelpers
+{
+    using System;
+
+    public class JsonSerializer<T>
+    {
+        string Serialize(T obj)
+        {
+
+        }
+    }
+}
