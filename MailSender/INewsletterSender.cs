168137f44e567edf75bfffdc31e28d9d75c7126b
konto <m.dobrzynski@outlook.com>
2014-05-22 23:34:25 +0200

    Revert "Update INewsletterSender.cs"
    This reverts commit f93305d1ea218b06b02068e75589c81713f5af7b.

--- MailSender/INewsletterSender.cs
+++ MailSender/INewsletterSender.cs
@@ -1,28 +1,12 @@
 // -----------------------------------------------------------------------
-//  <copyright file="ISender.cs" company="m (m.dobrzynski@outlook.com)">
+//  <copyright file="INewsletterSender.cs" company="m (m.dobrzynski@outlook.com)">
 //      Author: m (m.dobrzynski@outlook.com).
 //  </copyright>
 // -----------------------------------------------------------------------
 
 namespace MailSender
 {
-    using System.Collections.Generic;
-    
     interface INewsletterSender
     {
-        string CreateReceiversList(List<Receiver> receivers);
-
-        List<Receiver> GetReceiversLists();
-
-        string CreateCampaign(string listid, Mail mail);
-
-        string StartCampaign(string campaignid);
-
-        string PauseCampaign(string campaignid);
-
-        string ResumeCampaign(string campaignid);
-
-        string SearchCampaign(string query);
-
     }
 }
